0b03a9e9565e63b612875309646c9654222cfee4
SungWonHwang <swon0331@gmail.com>
2022-11-19 17:05:41 +0900

    [ADD]logoImageAndConnectSettingActivities

---
+++ app/src/main/java/com/example/designthinkingproject/SettingTextSizeActivity.java
@@ -0,0 +1,14 @@
+package com.example.designthinkingproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SettingTextSizeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting_text_size);
+    }
+}
