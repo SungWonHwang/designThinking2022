419cc7975160af0fddf6c0d0565a4a2e172f04a0
HwangSungWon <swon0331@gmail.com>
2022-11-19 15:38:36 +0900

    [ADD] MainActivitiesAndSplashActivity

---
+++ app/src/main/java/com/example/designthinkingproject/ChooseFoodActivity.java
@@ -0,0 +1,14 @@
+package com.example.designthinkingproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChooseFoodActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_food);
+    }
+}
--- app/src/main/java/com/example/designthinkingproject/MainActivity.java
+++ app/src/main/java/com/example/designthinkingproject/MainActivity.java
@@ -1,14 +1,69 @@
 package com.example.designthinkingproject;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.designthinkingproject.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot()); // 2
+
+
+        // btnCafePart 연결
+        binding.btnCafePart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ChooseCafeActivity.class );
+                startActivity(intent); //액티비티 이동
+                //Toast.makeText(MainActivity.this, "clicked button", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // btn_foodPart 연결
+        binding.btnFoodPart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ChooseFoodActivity.class );
+                startActivity(intent); //액티비티 이동
+            }
+        });
+
+        // btn_moviePart 연결
+        binding.btnMoviePart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ChooseMovieActivity.class );
+                startActivity(intent); //액티비티 이동
+            }
+        });
+
+        // btn_moviePart 연결
+        binding.btnMartPart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ChooseMartActivity.class );
+                startActivity(intent); //액티비티 이동
+            }
+        });
+
+        //Setting 연결
+        binding.btnSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddSettingActivity.class );
+                startActivity(intent); //액티비티 이동
+            }
+        });
+
     }
 }
