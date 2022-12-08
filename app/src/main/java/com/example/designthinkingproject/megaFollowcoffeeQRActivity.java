963311538c0859ca3a564d23a12833bec854cba8
Cheon's Desktop <lucy069052@khu.ac.kr>
2022-12-08 09:32:58 +0900

    메가 따라하기 파일 이름 변경2

--- app/src/main/java/com/example/designthinkingproject/megaFollowcoffeeQRActivity.java
+++ app/src/main/java/com/example/designthinkingproject/megaFollowcoffeeQRActivity.java
@@ -9,6 +9,6 @@ public class megaFollowcoffeeQRActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mega_followcoffee_qractivity);
+        setContentView(R.layout.activity_mega_follow_8qr);
     }
 }
