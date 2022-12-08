d27a8c6aeab326a5c1ce4e84f719166d82b00b1f
LimHeena <dlagmlsk8@gmail.com>
2022-12-08 12:39:55 +0900

    ㅎㅎ

--- app/src/main/java/com/example/designthinkingproject/DictionaryActivity.java
+++ app/src/main/java/com/example/designthinkingproject/DictionaryActivity.java
@@ -1,17 +1,12 @@
 package com.example.designthinkingproject;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 
 import com.example.designthinkingproject.databinding.ActivityDictionaryBinding;
-import com.example.designthinkingproject.databinding.ActivityStudyMega1Binding;
 
-public class Dictionary extends MyBaseActivity {
+public class DictionaryActivity extends MyBaseActivity {
 
     private ActivityDictionaryBinding binding;
 
---
+++ app/src/main/java/com/example/designthinkingproject/MyDictionaryActivity.java
@@ -0,0 +1,14 @@
+package com.example.designthinkingproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyDictionaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_dictionary);
+    }
+}
