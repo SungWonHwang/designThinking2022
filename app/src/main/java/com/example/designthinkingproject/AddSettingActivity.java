a1e44d26007777506c34d7095195ab61cb080ec8
SungWonHwang <swon0331@gmail.com>
2022-12-08 10:59:45 +0900

    [addAndFix] sound Setting

--- app/src/main/java/com/example/designthinkingproject/AddSettingActivity.java
+++ app/src/main/java/com/example/designthinkingproject/AddSettingActivity.java
@@ -2,7 +2,10 @@ package com.example.designthinkingproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.Toast;
 
@@ -11,12 +14,32 @@ import com.example.designthinkingproject.databinding.ActivityAddSettingBinding;
 public class AddSettingActivity extends MyBaseActivity {
 
     private ActivityAddSettingBinding binding;
+    MediaPlayer mediaPlayer;
 
+    // 액티비티가 종료될때.. 이곳을 실행함.
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityAddSettingBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        SharedPreferences voiceBool = getSharedPreferences("VoiceBool", MODE_PRIVATE);
+        Boolean voice = voiceBool.getBoolean("VoiceBool", false);
+
+        if(voice){
+        }
+        else{
+            mediaPlayer = MediaPlayer.create(AddSettingActivity.this, R.raw.presound);
+            mediaPlayer.start();
+        }
 
         //글자크기 설정 연결 버튼
         binding.btnTextSize.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/designthinkingproject/SettingVoiceServiceActivity.java
+++ app/src/main/java/com/example/designthinkingproject/SettingVoiceServiceActivity.java
@@ -2,13 +2,49 @@ package com.example.designthinkingproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import com.example.designthinkingproject.databinding.ActivitySettingVoiceServiceBinding;
+
 
 public class SettingVoiceServiceActivity extends MyBaseActivity {
+    private ActivitySettingVoiceServiceBinding binding;
+    private SharedPreferences voiceBool;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setting_voice_service);
+        binding = ActivitySettingVoiceServiceBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot()); // 2
+
+        voiceBool = getSharedPreferences("VoiceBool", MODE_PRIVATE);
+        SharedPreferences.Editor editor = voiceBool.edit();
+
+        //btnVoiceTrue 연결 버튼
+        binding.btnVoiceTrue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editor.putBoolean("VoiceBool", false);
+                editor.commit();
+                /*읽어오기
+                SharedPreferences pref = getSharedPreferences("pref", MODE_PRIVATE);
+                int nVal = pref.getInt("key2", 0);
+                */
+            }
+        });
+
+        //btnVoiceFalse 연결 버튼
+        binding.btnVoiceFalse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editor.putBoolean("VoiceBool", true);
+                editor.commit();
+            }
+        });
     }
+
 }
